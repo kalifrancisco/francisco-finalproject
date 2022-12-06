41a5e9bdc14eeab44f595e61a1385e3b5b64a05a
Kali Francisco <97408276+kalifrancisco@users.noreply.github.com>
2022-12-06 11:07:30 -0500

    Add files via upload

---
+++ js/kalijs.js
@@ -0,0 +1,40 @@
+//js for custom large cursor:
+
+let innerC = document.querySelector(".inner_cursor");
+let outerC = document.querySelector(".outer_cursor");
+
+document.addEventListener("mousemove", moveCurs);
+
+
+function moveCurs(e) {
+    let x = e.clientX;
+    let y = e.clientY;
+
+    console.log(x);
+    console.log(y);
+
+    innerC.style.left = `${x}px`;
+    innerC.style.top = `${y}px`;
+
+    outerC.style.left = `${x}px`;
+    outerC.style.top = `${y}px`;
+
+}
+
+
+//paralaz slide in effect:
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        } else {
+                    entry.target.classList.remove("show");
+                }
+        });
+ });
+
+
+ const hiddenTest = document.querySelectorAll(".hidden");
+
+hiddenElements.forEach((thing) => observer.observe(thing))
+
