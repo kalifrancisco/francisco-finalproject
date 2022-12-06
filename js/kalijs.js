e7ebdf143bb2c8dc954958c0f1c9e8d2799a7a39
Kali Francisco <97408276+kalifrancisco@users.noreply.github.com>
2022-12-06 11:18:06 -0500

    get rid of js show

--- js/kalijs.js
+++ js/kalijs.js
@@ -27,9 +27,7 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add("show");
-        } else {
-                    entry.target.classList.remove("show");
-                }
+        } 
         });
  });
 
